d066308bff29af0c80b85313aa7eb11c61b29896
piman51277 <piman.dev@gmail.com>
2024-07-11 03:51:18 -0400

    add html minifier

--- tools/build.js
+++ tools/build.js
@@ -1,6 +1,10 @@
 const nunjucks = require("nunjucks");
+const { minify } = require('html-minifier')
 const { writeFileSync } = require('fs');
 
 nunjucks.configure("njk");
 const output = nunjucks.render("index.njk");
-writeFileSync("dist/index.html", output);
+writeFileSync("dist/index.html", minify(output, {
+  collapseWhitespace: true,
+  removeComments: true
+}));
