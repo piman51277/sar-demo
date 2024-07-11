bc63ede7c9743718680385248eadfd670c8d61ec
piman51277 <piman.dev@gmail.com>
2024-07-10 23:21:14 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+//set the environment
+const env = process.env.NODE_ENV || "production";
+
+module.exports = {
+  entry: "src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            configFile: path.resolve(__dirname, "tscondif.json"),
+            transpileOnly: true,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new ForkTsCheckerWebpackPlugin()],
+  resolve: {
+    extensions: [".ts"],
+    symlinks: false,
+    cacheWithContext: false,
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: env,
+  devtool: env === "development" ? "eval" : false,
+  watchOptions: {
+    aggregateTimeout: 300,
+    ignored: /node_modules/
+  }
+};
